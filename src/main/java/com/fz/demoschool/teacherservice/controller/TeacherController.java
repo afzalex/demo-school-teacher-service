8525a66188df7f0eea6a3e51660c557dd764fe12
Mohammad Afzal <mafzal@calibo.com>
2022-05-18 03:03:11 +0530

    Example with teacher service replica set

--- src/main/java/com/fz/demoschool/teacherservice/controller/TeacherController.java
+++ src/main/java/com/fz/demoschool/teacherservice/controller/TeacherController.java
@@ -1,8 +1,9 @@
 package com.fz.demoschool.teacherservice.controller;
 
-import com.fz.demoschool.core.dto.TeacherModel;
+import com.fz.demoschool.core.dto.TeacherDto;
 import com.fz.demoschool.teacherservice.service.TeacherService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,35 +16,38 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/teacher")
+@Slf4j
 public class TeacherController {
 
+    private static final String SERVICE_UUID = UUID.randomUUID().toString();
     private final TeacherService teacherService;
 
-    private static final Map<Integer, List<TeacherModel>> teacherListMap =  Map.of(
+    private static final Map<Integer, List<TeacherDto>> teacherListMap =  Map.of(
             1, List.of(
-                    TeacherModel.builder().uuid(UUID.randomUUID().toString()).name("T1").build(),
-                    TeacherModel.builder().uuid(UUID.randomUUID().toString()).name("T2").build(),
-                    TeacherModel.builder().uuid(UUID.randomUUID().toString()).name("T3").build()
+                    TeacherDto.builder().uuid(UUID.randomUUID().toString()).name("T11").schoolId(1).build(),
+                    TeacherDto.builder().uuid(UUID.randomUUID().toString()).name("T12").schoolId(1).build(),
+                    TeacherDto.builder().uuid(UUID.randomUUID().toString()).name("T13").schoolId(1).build()
             ),
             2, List.of(
-                    TeacherModel.builder().uuid(UUID.randomUUID().toString()).name("T21").build(),
-                    TeacherModel.builder().uuid(UUID.randomUUID().toString()).name("T22").build()
+                    TeacherDto.builder().uuid(UUID.randomUUID().toString()).name("T21").schoolId(2).build(),
+                    TeacherDto.builder().uuid(UUID.randomUUID().toString()).name("T22").schoolId(2).build()
             ),
             3, List.of(
-                    TeacherModel.builder().uuid(UUID.randomUUID().toString()).name("T31").build(),
-                    TeacherModel.builder().uuid(UUID.randomUUID().toString()).name("T32").build(),
-                    TeacherModel.builder().uuid(UUID.randomUUID().toString()).name("T33").build(),
-                    TeacherModel.builder().uuid(UUID.randomUUID().toString()).name("T34").build()
+                    TeacherDto.builder().uuid(UUID.randomUUID().toString()).name("T34").schoolId(3).build(),
+                    TeacherDto.builder().uuid(UUID.randomUUID().toString()).name("T31").schoolId(3).build(),
+                    TeacherDto.builder().uuid(UUID.randomUUID().toString()).name("T32").schoolId(3).build(),
+                    TeacherDto.builder().uuid(UUID.randomUUID().toString()).name("T33").schoolId(3).build()
             )
     );
 
     @GetMapping("/ping")
     public String ping() {
-        return "This is a message from teacher microservice";
+        log.info("{} : Ping is working properly in Teacher Service.", SERVICE_UUID);
+        return String.format("%s : Ping is working properly in Teacher Service", SERVICE_UUID);
     }
 
     @GetMapping("/{schoolId}")
-    public List<TeacherModel> getTeacherList(@PathVariable("schoolId") Integer schoolId) {
+    public List<TeacherDto> getTeacherList(@PathVariable("schoolId") Integer schoolId) {
         return teacherListMap.get(schoolId);
     }
 
